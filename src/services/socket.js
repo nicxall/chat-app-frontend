fe5c215399b3191dcbf8673f9bccb5e39fcd66b1
juan <cuentapersonaluni@gmail.com>
2024-07-30 07:04:39 +0200

    'update port in socket'

--- src/services/socket.js
+++ src/services/socket.js
@@ -3,7 +3,8 @@ import { io } from 'socket.io-client'
 
 //export const socket = io('http://localhost:81') 
 
+const SOCKET_URL = process.env.URL_BACKEND || 'http://localhost';
 
-export const socket = io(process.env.URL_BACKEND, {
+export const socket = io(SOCKET_URL, {
 	transports: ['websocket'], // Asegúrate de usar el transporte adecuado
 });
