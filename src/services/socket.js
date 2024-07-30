3c131ccecc290d138f1e28e698ce6fb18616a0e0
juan <cuentapersonaluni@gmail.com>
2024-07-30 05:07:19 +0200

    add dominio de escucha con el backend

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,4 +1,8 @@
 import { io } from 'socket.io-client'
 
 
-export const socket = io('http://localhost:81') 
+//export const socket = io('http://localhost:81') 
+
+export const socket = io(process.env.URL_BACKEND, {
+	transports: ['websocket'], // Asegúrate de usar el transporte adecuado
+});
