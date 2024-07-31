349fdb6043e4b5c101af4298d76e71c3ba81d59f
juan <cuentapersonaluni@gmail.com>
2024-07-31 04:46:19 +0200

    'add env'

--- src/services/socket.js
+++ src/services/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client'
 
 //export const socket = io('http://localhost:81') 
 
-const SOCKET_URL = process.env.VITE_VERCEL_ENV || 'http://localhost:3000';
+const SOCKET_URL = process.env.URL_BACKEND || 'http://localhost:3000';
 
 export const socket = io(SOCKET_URL, {
 	transports: ['websocket'], // Asegúrate de usar el transporte adecuado
