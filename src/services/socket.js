515f7e3e8c40ed74fdf9ed7ab92dd3077adfb3fd
juan <cuentapersonaluni@gmail.com>
2024-07-31 05:04:03 +0200

    'upodate socket url'

--- src/services/socket.js
+++ src/services/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client'
 
 //export const socket = io('http://localhost:81') 
 
-const SOCKET_URL =' https://chat-app-backend-abmj.onrender.com'
+const SOCKET_URL = process.env.URL;
 
 export const socket = io(SOCKET_URL, {
 	transports: ['websocket'], // Asegúrate de usar el transporte adecuado
