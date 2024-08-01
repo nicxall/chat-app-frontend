a840842b9a2f07002c51e0007a0e6fe14fff288e
juan <cuentapersonaluni@gmail.com>
2024-08-01 05:07:47 +0200

    add enviroment variable

--- src/services/socket.js
+++ src/services/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client'
 
 //export const socket = io('http://localhost:81') 
 
-const SOCKET_URL = process.env.URL;
+const SOCKET_URL = process.env.NODE_ENV === 'production' ? process.env.SOCKET_URL : 'localhost:3000';
 
 export const socket = io(SOCKET_URL, {
 	transports: ['websocket'], // Asegúrate de usar el transporte adecuado
