6f297100f209eb94607278b8e1514ffdaf05580f
juan <cuentapersonaluni@gmail.com>
2024-08-02 02:13:40 +0200

    cambios en los env

--- src/services/socket.js
+++ src/services/socket.js
@@ -2,11 +2,12 @@ import { io } from 'socket.io-client'
 
 
 //export const socket = io('http://localhost:81') 
-
-const SOCKET_URL = process.env.NODE_ENV === 'production' ? process.env.SOCKET_URL : 'localhost:3000';
+const SOCKET_URL = import.meta.env.PROD
+	? import.meta.env.VITE_SOCKET_URL
+	: 'http://localhost:3000';
 
 export const socket = io(SOCKET_URL, {
 	transports: ['websocket'], // Asegúrate de usar el transporte adecuado
 });
 
-console.log(`En produccion se ejecuta en la url: ${process.env.SOCKET_URL}`)
+console.log(`En producción se ejecuta en la URL: ${import.meta.env.VITE_SOCKET_URL}`);
