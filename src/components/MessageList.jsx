6525ad91046de249d294f8dc0fbb4fb3b1caa9a7
juan <cuentapersonaluni@gmail.com>
2024-08-05 03:02:42 +0200

    'componentes modificados para ser responsives'

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -20,18 +20,18 @@ const MessageList = ({ messages, currentUserId, bottomRef }) => {
           className={`flex ${msg.sender.id === currentUserId ? 'justify-end' : 'justify-start'} items-start`}
         >
           {msg.sender.id !== currentUserId && (
-            <div className="w-10 h-10 mr-2 flex items-center justify-center rounded-full bg-gray-500 text-white">
+            <div className="w-10 h-10 mr-2 flex items-center justify-center rounded-full bg-gray-500 text-white sm:w-8 sm:h-8">
               {getInitials(msg.sender.username)}
             </div>
           )}
           <div
-            className={`rounded-lg px-4 py-2 max-w-xs ${msg.sender.id === currentUserId ? 'bg-blue-700 text-white' : 'bg-gray-700 text-white'}`}
+            className={`rounded-lg px-4 py-2 max-w-xs ${msg.sender.id === currentUserId ? 'bg-blue-700 text-white' : 'bg-gray-700 text-white'} sm:px-2 sm:py-1 sm:text-xs`}
           >
-            <div className="font-bold">{msg.sender.username}</div>
-            <div className="font-normal text-gray-700 dark:text-gray-300">{msg.content}</div>
+            <div className="font-bold sm:text-sm">{msg.sender.username}</div>
+            <div className="font-normal text-gray-700 dark:text-gray-300 sm:text-xs">{msg.content}</div>
           </div>
           {msg.sender.id === currentUserId && (
-            <div className="w-10 h-10 ml-2 flex items-center justify-center rounded-full bg-blue-500 text-white">
+            <div className="w-10 h-10 ml-2 flex items-center justify-center rounded-full bg-blue-500 text-white sm:w-8 sm:h-8">
               {getInitials(msg.sender.username)}
             </div>
           )}
